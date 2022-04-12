984934ef3b510496d4bdcfcf6d7acbdb51e05baf
Lakshay Karnwal <lakshaykarnwal@gmail.com>
2022-04-12 09:28:18 +0530

    downloaded images using flickr-images script

--- flickr-images.py
+++ flickr-images.py
@@ -1,9 +1,10 @@
 import flickrapi
+import urllib.request
 
 api_key = '636bd872cd404868531ed627c53f61f3'
 api_secret = 'be7d5fa682f09280'
 
-def searchFlickrImages(term, category='architecture'):
+def searchFlickrImages(term, category='pets'):
     global api_key, api_secret
     urls = []
     flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json')
@@ -11,14 +12,32 @@ def searchFlickrImages(term, category='architecture'):
 
     photos = returnedImages.get('photos').get('photo')
     for photo in photos:
-        farm = type(photo.get('farm'))
-        server = type(photo.get('server'))
-        id = type(photo.get('id'))
-        secret = type(photo.get('secret'))
+        farm = photo.get('farm')
+        server = photo.get('server')
+        id = photo.get('id')
+        secret = photo.get('secret')
         url = 'http://farm' + f'{farm}' + '.static.flickr.com/' + server + '/' + id + '_' + secret + '_m.jpg'
-        print(url)
+        urls.append(url)
+        #print(type(farm))
     
-    return(returnedImages.get('photos').get('photo')[0].get('id'))
+    return(urls)
 
-output = searchFlickrImages('Tree')
-print(output)
+def url_to_jpg(i, url, file_path, saved_name):
+    filename = 'image-{}{}.jpg'.format(saved_name,i)
+    full_path = '{}{}'.format(file_path, filename)
+    urllib.request.urlretrieve(url, full_path)
+
+    print('{} saved.'.format(filename))
+    return None
+
+urls_cute = searchFlickrImages('cute')
+file_path = 'images/cute_pets/'
+saved_name = 'cutePet'
+counter = 0
+
+for url in urls_cute:
+    url_to_jpg(counter, url, file_path, saved_name)
+    print("saved")
+    counter = counter + 1
+
+print("completed")
